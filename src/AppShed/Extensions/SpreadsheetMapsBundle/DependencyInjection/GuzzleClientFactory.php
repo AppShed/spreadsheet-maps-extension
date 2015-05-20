4fa6041c8c846e094ae40bcc29479e4910296f54
victor <stelsvitya@gmail.com>
2015-05-20 16:23:46 +0300

    fix composer

--- src/AppShed/Extensions/SpreadsheetMapsBundle/DependencyInjection/GuzzleClientFactory.php
+++ src/AppShed/Extensions/SpreadsheetMapsBundle/DependencyInjection/GuzzleClientFactory.php
@@ -4,10 +4,7 @@
  */
 namespace AppShed\Extensions\SpreadsheetMapsBundle\DependencyInjection;
 
-use Doctrine\Common\Cache\ApcCache;
 use GuzzleHttp\Client;
-use GuzzleHttp\Subscriber\Cache\CacheStorage;
-use GuzzleHttp\Subscriber\Cache\CacheSubscriber;
 
 class GuzzleClientFactory
 {
